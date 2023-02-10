48a5f3c5c3fa4a686efa84a263e4a8005f54e29f
ilnvladand <ilnvladand@yandex.ru>
2023-02-11 03:05:44 +1000

    выполнмл дз1

---
+++ hw1/Class2/Class2.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Class2;
+
+class Class2
+{
+    private string $text;
+
+    public function __construct(string $text)
+    {
+        $this->text = $text;
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+}
---
+++ hw1/Class3/Class/3.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Class3;
+
+class Class_3
+{
+    private string $text;
+
+    public function __construct(string $text)
+    {
+        $this->text = $text;
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+}
