fb202e6a195e979fdf607d079a0a090feab9648f
ilnvladand <ilnvladand@yandex.ru>
2023-02-14 23:29:58 +1000

    выполнил ДЗ2

---
+++ hw2/cli.php
@@ -0,0 +1,25 @@
+<?php
+
+use GeekBrains\LevelTwo\Blog\Command\Arguments;
+use GeekBrains\LevelTwo\Blog\Command\CreateUserCommand;
+use GeekBrains\LevelTwo\Blog\Repositories\UsersRepository\UsersRepositoryInterface;
+use GeekBrains\LevelTwo\Blog\Repositories\UsersRepository\SqliteUsersRepository;
+use GeekBrains\LevelTwo\Blog\Repositories\PostsRepository\SqlitePostsRepository;
+use GeekBrains\LevelTwo\Blog\Repositories\CommentsRepository\SqliteCommentsRepository;
+
+include __DIR__ . "/vendor/autoload.php";
+
+//Создаём объект подключения к SQLite
+$connection = new PDO('sqlite:' . __DIR__ . '/blog.sqlite');
+
+$usersRepository = new SqliteUsersRepository($connection);
+$postRepository = new SqlitePostsRepository($connection);
+$commentRepository = new SqliteCommentsRepository($connection);
+
+$command = new CreateUserCommand($usersRepository);
+
+try {
+    $command->handle(Arguments::fromArgv($argv));
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
